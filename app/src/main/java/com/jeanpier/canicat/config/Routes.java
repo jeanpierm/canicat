d40f240ccfec0cc64e23d551f4bc62a97629715f
Jeanpier Mendoza <jeanpi3rm@gmail.com>
2021-09-02 01:05:14 -0500

    refactor: Actualizar configuración de rutas para entorno de producción

--- app/src/main/java/com/jeanpier/canicat/config/Routes.java
+++ app/src/main/java/com/jeanpier/canicat/config/Routes.java
@@ -2,16 +2,14 @@ package com.jeanpier.canicat.config;
 
 public class Routes {
     // Para ver tu ip local ejecuta el comando "ipconfig" en cmd/powershell. (IPv4)
-    private static final String IP_LOCAL = "192.168.100.195";
-//    private static final String IP_LOCAL = "canicat-backend.herokuapp.com";
-//    private static final String PORT = "21651";
+//    private static final String DOMAIN = "192.168.100.195";
+    private static final String DOMAIN = "canicat-backend.herokuapp.com";
+//    puerto solo usado para desarrollo, NO en producción
     private static final String PORT = "3000";
-    public static final String API_APTH = "/api";
+    public static final String API_PATH = "/api";
     public static final String PETS_PATH = "/pets";
     public static final String USERS_PATH = "/users";
     public static final String VACCINE_PATH = "/vaccines";
     public static final String LOGIN_PATH = "/auth/login";
-    public static final String BASE_URI = "http://" + IP_LOCAL + ":" + PORT;
-//    public static final String BASE_URI = "https://" + IP_LOCAL;
-    public static final String API_URI = BASE_URI + "/api/";
+    public static final String BASE_URI = "https://" + DOMAIN;
 }
